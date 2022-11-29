13b67d195c0aef21999fae4dcf9e65d865dd5c45
Khvicha Ostatishvili <kh.ostatishvili@Kh-ostatishvilis-MacBook-Air.local>
2022-11-29 16:14:01 +0400

    fix(style): adding gaps to select all and unselct all buttons

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -9,37 +9,8 @@
         </div>
         <div class="w-3/4 py-4 break-words">
             <slot name="value" class="text-90 flex">
-                <span
-                    class="w-full flex space-2 max-col-2"
-                    v-if="field.withGroups"
-                >
-                    <div
-                        v-for="(permissions, group) in field.options"
-                        :key="group"
-                        class="mb-4 ml-3 border p-2"
-                    >
-                        <h3 class="my-2 font-bold">{{ __(group) }}</h3>
-                        <div
-                            v-for="(permission, option) in permissions"
-                            :key="option"
-                            class="flex-auto mb-2"
-                        >
-                            <span :class="optionClass(permission)">{{
-                                permission.label
-                            }}</span>
-                        </div>
-                    </div>
-                </span>
-                <span class="w-full max-col-2" v-else>
-                    <div
-                        v-for="(label, option) in field.options"
-                        :key="option"
-                        class="flex-auto"
-                    >
-                        <span :class="optionClass(permission)">{{
-                            label
-                        }}</span>
-                    </div>
+                <span>
+                    {{ `${field.value.length} permissions attached this role` }}
                 </span>
             </slot>
         </div>
@@ -64,18 +35,3 @@ export default {
     },
 };
 </script>
-
-<style>
-.max-col-3 {
-    -moz-column-count: 3;
-    -webkit-column-count: 3;
-    column-count: 3;
-    white-space: nowrap;
-}
-.max-col-2 {
-    -moz-column-count: 2;
-    -webkit-column-count: 2;
-    column-count: 2;
-    white-space: nowrap;
-}
-</style>
--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -13,7 +13,7 @@
                     >{{ __("Select all") }}</span
                 >
                 <span
-                    class="ml-4 shadow relative bg-primary-500 hover:bg-primary-400 text-white dark:text-gray-900 cursor-pointer rounded text-sm font-bold focus:outline-none focus:ring ring-primary-200 dark:ring-gray-600 inline-flex items-center justify-center h-9 px-3 shadow relative bg-primary-500 hover:bg-primary-400 text-white dark:text-gray-900"
+                    class="ml-3 shadow relative bg-primary-500 hover:bg-primary-400 text-white dark:text-gray-900 cursor-pointer rounded text-sm font-bold focus:outline-none focus:ring ring-primary-200 dark:ring-gray-600 inline-flex items-center justify-center h-9 px-3 shadow relative bg-primary-500 hover:bg-primary-400 text-white dark:text-gray-900"
                     @click="uncheckAll()"
                     >{{ __("Do not select any") }}</span
                 >
